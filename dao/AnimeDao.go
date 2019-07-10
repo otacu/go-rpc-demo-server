43bb8a50471dea2f661d238fdaf87b54c3254775
JingshengYe <jingsheng-ye@msyc.cc>
2019-07-10 17:44:49 +0800

    :ok_hand: 修正数据库连接池的用法

--- dao/AnimeDao.go
+++ dao/AnimeDao.go
@@ -5,22 +5,15 @@ import (
 	"errors"
 	_ "github.com/go-sql-driver/mysql"
 	proto "go-rpc-demo-server/proto"
-	"log"
-)
-
-const (
-	dataSource = "root:123456@tcp(127.0.0.1:3306)/python?charset=utf8"
 )
 
 type AnimeDao struct {
+	DB *sql.DB
 }
 
 func (a *AnimeDao) QueryByAnimeId(animeId int32, res *proto.QueryAnimeInfoResponse) error {
-	db, err0 := a.OpenDB()
-	if err0 != nil {
-		return err0
-	}
-	stmt, err1 := db.Prepare("select anime_id, en_name from tb_myanimelist_anime where anime_id=?")
+	stmt, err1 := a.DB.Prepare("select anime_id, en_name from tb_myanimelist_anime where anime_id=?")
+	// 表示return后关闭连接
 	defer stmt.Close()
 	if err1 != nil {
 		return errors.New("prepare error")
@@ -35,19 +28,3 @@ func (a *AnimeDao) QueryByAnimeId(animeId int32, res *proto.QueryAnimeInfoRespon
 	return nil
 }
 
-func (a *AnimeDao) OpenDB() (db *sql.DB, err error) {
-	db, err1 := sql.Open("mysql", dataSource)
-	if err1 != nil {
-		return db, errors.New("open database fail")
-	}
-	//设置数据库最大连接数
-	db.SetConnMaxLifetime(100)
-	//设置上数据库最大闲置连接数
-	db.SetMaxIdleConns(10)
-	//验证连接
-	if err2 := db.Ping(); err2 != nil {
-		return db, errors.New("ping database fail")
-	}
-	log.Println("connect success")
-	return db, nil
-}
--- main.go
+++ main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"github.com/micro/go-micro"
 	"go-rpc-demo-server/dao"
 	proto "go-rpc-demo-server/proto"
 	"log"
 )
 
+const (
+	dataSource = "root:123456@tcp(127.0.0.1:3306)/python?charset=utf8"
+)
+
+// 数据库连接池
+var DB *sql.DB
+
 type AnimeService struct {
 }
 
 func (s *AnimeService) QueryAnimeInfo(ctx context.Context, req *proto.QueryAnimeInfoRequest, res *proto.QueryAnimeInfoResponse) error {
-	animeDao := dao.AnimeDao{}
+	var animeDao = dao.AnimeDao{DB}
 	err := animeDao.QueryByAnimeId(req.AnimeId, res)
 	if err != nil {
 		return err
@@ -22,15 +31,41 @@ func (s *AnimeService) QueryAnimeInfo(ctx context.Context, req *proto.QueryAnime
 
 // go run main.go
 func main() {
+	// 这里的open是创建数据库连接池，并不是打开连接，查询时才建立连接
+	db, err := OpenDB()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	DB = db
+
 	service := micro.NewService(
 		micro.Name("AnimeService"),
 		micro.Version("latest"),
 	)
 	service.Init()
+	// 服务默认注册在mdns，可以改成其它注册中心
 	proto.RegisterAnimeServiceHandler(service.Server(), new(AnimeService))
 
 	// Run the server
 	if err := service.Run(); err != nil {
 		log.Println(err)
 	}
+}
+
+func OpenDB() (db *sql.DB, err error) {
+	db, err1 := sql.Open("mysql", dataSource)
+	if err1 != nil {
+		return nil, errors.New("open database fail")
+	}
+	//设置数据库最大连接数
+	db.SetConnMaxLifetime(100)
+	//设置上数据库最大闲置连接数
+	db.SetMaxIdleConns(10)
+	//验证连接
+	if err2 := db.Ping(); err2 != nil {
+		return nil, errors.New("ping database fail")
+	}
+	log.Println("connect success")
+	return db, nil
 }
